5ba2f9cb53b7d98f81b1414ecbcedb50d374b038
andrewDoesIt <151869033+andrewdoesit@users.noreply.github.com>
2024-05-14 19:25:49 -0400

    Update TerminalFarkle.rb

--- TerminalFarkle/lib/TerminalFarkle.rb
+++ TerminalFarkle/lib/TerminalFarkle.rb
@@ -1,53 +1,16 @@
 # frozen_string_literal: true
 
 require_relative "TerminalFarkle/version"
+require_relative "Dice.rb"
+#require_relative "GameLogic.rb"
 
 module TerminalFarkle
-  class Game
-    attr_accessor :dice
-
-    def initialize
-      @dice = []
-    end
-
-    def rollDice
-      @dice = []
-      6.times { dice << rand(1..6) }
-      puts dice
-    end
-
-    def countDice
-      @ones = @dice.count(1)
-      @twos = @dice.count(2)
-      @threes = @dice.count(3)
-      @fours = @dice.count(4)
-      @fives = @dice.count(5)
-      @sixes = @dice.count(6)
-      puts "Ones: #{@ones}, Twos: #{@twos}, Threes: #{@threes}, Fours: #{@fours}, Fives: #{@fives}, Sixes: #{@sixes}"
-    end
-
-    def checkPoints
-      if @ones == 1
-        puts "you have a 100 points"
-        @points += 100
-      else 
-        puts "you have no points"
-      end
-    end
-  end
-
-  class GameLogic
+  class PlayGame
     attr_accessor :points
 
     def initialize
-      game = TerminalFarkle::Game.new
-      game.rollDice
-      game.countDice
-      game.checkPoints
+        @points = 0
     end
-  end
-
-  class PlayGame
 
     def play
       puts "welcome to TerminalFarkle"
@@ -56,13 +19,11 @@ module TerminalFarkle
         choice = gets.chomp.downcase
     
         if choice == "y"
-          game = TerminalFarkle::Game.new
+          game = TerminalFarkle::Dice.new
           game.rollDice
           game.countDice
-          game.checkPoints
-
         elsif choice == "n"
-          puts "Okay, thanks for playing!"
+          puts "Thanks for playing!"
           break
         else
           puts "Invalid choice, please try again."
@@ -72,11 +33,6 @@ module TerminalFarkle
     end
   end
 end
-
-#game = TerminalFarkle::GameLogic.new
-#game.checkPoints
-#game.rollDice
-#game.countDice
 
 playgame = TerminalFarkle::PlayGame.new
 playgame = playgame.play
